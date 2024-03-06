551a3fbfa48fd124103851ac04766e017d7573eb
Craig Hudson <craighudson1211@gmail.com>
2024-03-06 19:25:48 +0000

    update prodcut admin interface

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Size
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'is_available',
+    )
+
+    list_filter = ('category', 'is_available',)
+    search_fields = ('name', 'description',)
+    filter_horizontal = ('sizes',)
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Size, SizeAdmin)
