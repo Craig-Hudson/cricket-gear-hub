5b05b81933ada7bb6521d78dc6be0f5dd0607705
Craig Hudson <craighudson1211@gmail.com>
2024-03-11 23:48:40 +0000

    use queries to filter categories and link them in templates

--- products/models.py
+++ products/models.py
@@ -28,6 +28,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     sizes = models.ManyToManyField(Size)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     is_available = models.BooleanField(default=True)
