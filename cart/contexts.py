7d24a3dc5560608f12f7eba6ba095d2a34159594
Craig Hudson <craighudson1211@gmail.com>
2024-03-21 22:12:22 +0000

    set up context processer and add views and templates for shopping cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,28 +1,39 @@
+from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
 def cart_contents(request):
-
     cart_items = []
-    total = 0
+    total = Decimal('0')
     product_count = 0
+    delivery = Decimal('3.99')
     cart = request.session.get('cart', {})
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        subtotal_for_item = quantity * product.price  
+        total += subtotal_for_item  
         product_count += quantity
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'subtotal': subtotal_for_item,
         })
+
+    # Add delivery fee to the total
+    total += delivery
+
+    # Calculate grand total
+    grand_total = total + delivery
 
     context = {
         'cart_items' : cart_items,
         'total' : total,
-        'product_count' : product_count
+        'product_count' : product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
     }
 
     return context
