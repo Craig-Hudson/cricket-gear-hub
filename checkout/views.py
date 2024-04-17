0d198844baaeb65bb623b9cabd08baa192ce2ab5
Craig Hudson <craighudson1211@gmail.com>
2024-04-17 10:19:47 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key' : 
+        'pk_test_51P6V2QRunzMTc6Q0D6EQMnI3wGwSQORfPhQZcTWLZB8UvgoKweaJOi1XVntS3cWURHB13F7JcjMeOIT1YVKoiPud00yRWFJ1VI',
+        'client_secret' : 'test client secret'
     }
 
     return render(request, template, context)
