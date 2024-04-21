495b413f9fd2151a93d48dcedeed5d38c1e7f4af
Craig Hudson <craighudson1211@gmail.com>
2024-04-21 22:09:29 +0000

    add edit products template, view and url

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.individual_product, name='individual_product'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+
 ]
