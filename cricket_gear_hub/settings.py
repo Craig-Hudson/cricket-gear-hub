ca2a1bb4580c8e37aac5e6f5bf489497d883eabe
Craig Hudson <craighudson1211@gmail.com>
2024-04-25 22:12:44 +0000

    added a new secret key

--- cricket_gear_hub/settings.py
+++ cricket_gear_hub/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEVELOPMENT in os.environ
 
 ALLOWED_HOSTS = ['8000-craighudson-cricketgear-eidjouv5aki.ws-eu110.gitpod.io',
                 'cricket-gear-hub@herokuapp.com', 'localhost'        
