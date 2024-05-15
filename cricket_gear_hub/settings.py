4d67ce66afbb3021647d416b43806f55c27ad892
Craig Hudson <craighudson1211@gmail.com>
2024-05-15 18:46:30 +0000

    adjust account verifaction and delete uneeded email settings

--- cricket_gear_hub/settings.py
+++ cricket_gear_hub/settings.py
@@ -100,16 +100,9 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USER = 'craighudson1211@gmail.com'
-EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD', '')
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
