aeb1472243ad103d79af9c8380f04035e794a851
Craig Hudson <craighudson1211@gmail.com>
2024-05-15 16:45:50 +0000

    take off button on description and display 3 words of description while enabling modal still.

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from products.models import Product
 # Create your views here.
 
 def index(request):
-    """ A view to return the index page and display the 4 newest products on the home page """
+    """ A view to return the index page and display the 4 newest products as featured products on the home page """
     featured_products = Product.objects.order_by('-id')[:4]
 
     context = {
